0b40811462ef329641c88f2f5176ad41c53412f9
mvtercero <mvtercero85@gmail.com>
2018-04-25 10:50:03 +0200

    Get seconds with js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 
 function setDate(){
     const now = new Date();
-    const seconds = now.getSeconds();
-    console.log(seconds);
+    const seconds = now.getSeconds(); //segundos del minuto actual
+    const secondsDegrees = ((seconds / 60)* 360); //segundos divididos por 60 multiplicados por 360
 }
 
 setInterval (setDate, 1000);
